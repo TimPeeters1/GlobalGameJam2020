bea61e5cea7c097965cc9989e8c91d9e72a3bb57
DennisBorst <dennis.borst@hotmail.nl>
2020-02-02 00:31:48 +0100

    Some changes in script

--- GGJ_2020/Assets/_Levels/Dennis/TrashBin.cs
+++ GGJ_2020/Assets/_Levels/Dennis/TrashBin.cs
@@ -7,18 +7,14 @@ public class TrashBin : Interactable
     [Header("Trash")]
     //private serialized
     [SerializeField] private int maxTrash;
-    [SerializeField] private float trashMaxY;
-    [SerializeField] private float trashMinY;
-    [SerializeField] private GameObject trash;
+    [SerializeField] private GameObject[] trash;
 
     //private
     private int trashCount;
-    private float increaseY;
 
     public override void Awake()
     {
         base.Awake();
-        increaseY = ((trashMaxY - trashMinY) / maxTrash);
     }
     public override void Update()
     {
@@ -27,31 +23,37 @@ public class TrashBin : Interactable
         if(trashCount > (maxTrash * 0.75f))
         {
             ProblemList.Instance.UpdateList(interactableNumber, 4);
+            trash[2].SetActive(true);
+            trash[1].SetActive(false);
         }
         else if(trashCount > (maxTrash * 0.50f))
         {
             ProblemList.Instance.UpdateList(interactableNumber, 3);
+            trash[1].SetActive(true);
+            trash[0].SetActive(false);
         }
         else if (trashCount > (maxTrash * 0.25f))
         {
             ProblemList.Instance.UpdateList(interactableNumber, 2);
+            trash[0].SetActive(true);
         }
         else
         {
             ProblemList.Instance.UpdateList(interactableNumber, 1);
+            trash[0].SetActive(false);
+            trash[1].SetActive(false);
+            trash[2].SetActive(false);
         }
     }
     public override void Interact()
     {
         trashCount = 0;
-        trash.transform.position = new Vector3(trash.transform.position.x, trashMinY, trash.transform.position.z);
     }
 
     public void AddTrash()
     {
         trashCount++;
 
-        trash.transform.position = new Vector3(trash.transform.position.x, trash.transform.position.y + increaseY, trash.transform.position.z);
         if(trashCount == maxTrash)
         {
             Debug.Log("Trashcan overloaded");
