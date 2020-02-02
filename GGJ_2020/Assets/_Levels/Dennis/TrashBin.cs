c399f135d79280ba673e9ba495e664ef839bea6f
DennisBorst <dennis.borst@hotmail.nl>
2020-02-02 11:42:14 +0100

    Trash bug fixed

--- GGJ_2020/Assets/_Levels/Dennis/TrashBin.cs
+++ GGJ_2020/Assets/_Levels/Dennis/TrashBin.cs
@@ -7,16 +7,24 @@ public class TrashBin : Interactable
     [Header("Trash")]
     //private serialized
     [SerializeField] private int maxTrash;
+    [SerializeField] private float dipositAnimationTime;
     [SerializeField] private GameObject[] trash;
 
     //private
-    private int trashCount = 5;
+    private int trashCount;
+    private float trashDissappearTime;
+    private bool animationPlaying;
     private Animator anim;
 
     public override void Awake()
     {
         base.Awake();
         anim = GetComponent<Animator>();
+        trashDissappearTime = dipositAnimationTime;
+
+        trash[0].SetActive(false);
+        trash[1].SetActive(false);
+        trash[2].SetActive(false);
     }
     public override void Update()
     {
@@ -48,15 +56,30 @@ public class TrashBin : Interactable
         else
         {
             ProblemList.Instance.UpdateList(interactableNumber, 1);
-            trash[0].SetActive(false);
-            trash[1].SetActive(false);
-            trash[2].SetActive(false);
+
+        }
+
+        if (animationPlaying)
+        {
+            trashDissappearTime = Timer(trashDissappearTime);
+
+            if (trashDissappearTime <= 0)
+            {
+                trashDissappearTime = dipositAnimationTime;
+                animationPlaying = false;
+
+                ProblemList.Instance.UpdateList(interactableNumber, 1);
+                trash[0].SetActive(false);
+                trash[1].SetActive(false);
+                trash[2].SetActive(false);
+            }
         }
     }
     public override void Interact()
     {
         trashCount = 0;
         anim.SetTrigger("Deposit");
+        animationPlaying = true;
     }
 
     public void AddTrash()
@@ -68,5 +91,11 @@ public class TrashBin : Interactable
             Debug.Log("Trashcan overloaded");
             ShipManagement.Instance.GameOver();
         }
+    }
+
+    private float Timer(float timer)
+    {
+        timer -= Time.deltaTime;
+        return timer;
     }
 }
